4fefd91715e9da58caaf8aa0146f350ced2ed52b
Leonardo Overbeck da Silva <overbecksilva.leonardo@gmail.com>
2022-02-20 13:27:47 -0300

    Adjusting GetCourses returned body

--- college-management-back/Domain/Api/GetCoursesApiResultFormatter.cs
+++ college-management-back/Domain/Api/GetCoursesApiResultFormatter.cs
@@ -13,38 +13,28 @@ namespace college_management_back.Domain.Api
 
             courses.ForEach(course =>
             {
-                List<ExpandoObject> subjectsDto = new List<ExpandoObject>();
-
+                Dictionary<int, ExpandoObject> studentsDto = new Dictionary<int, ExpandoObject>();
                 course.Subjects.ToList().ForEach(sub => {
-                    dynamic subObj = new ExpandoObject();
-                    subObj.SubjectID = sub.SubjectID;
-                    subObj.CourseID = sub.CourseID;
-                    subObj.TeacherID = sub.TeacherID;
-                    subObj.Description = sub.Description;
 
-                    List<ExpandoObject> studentsDto = new List<ExpandoObject>();
+                    foreach (var stu in sub.Students.ToList())
+                    {
+                        if (studentsDto.ContainsKey(stu.StudentID))
+                        {
+                            continue;
+                        }
 
-                    sub.Students.ToList().ForEach(stu => {
                         dynamic stuObj = new ExpandoObject();
-                        stuObj.Name = stu.Name;                   
-                        stuObj.Grade = stu.Grades
-                            .Where(g => g.StudentID == stu.StudentID && g.SubjectID == sub.SubjectID)
-                            .FirstOrDefault()
-                            .Value;
+                        stuObj.Name = stu.Name;
 
-                        studentsDto.Add(stuObj);
-                    });
-
-                    subObj.Students = studentsDto;
-
-
-                    subjectsDto.Add(subObj);
+                        stuObj.AvgGrade = stu.Grades.Average(g => g.Value);
+                        studentsDto.Add(stu.StudentID, stuObj);
+                    }
                 });
 
                 dynamic courseObj = new ExpandoObject();
-                courseObj.CourseID = course.CourseID;
+                courseObj.CourseId = course.CourseID;
                 courseObj.Description = course.Description;
-                courseObj.Subjects = subjectsDto;
+                courseObj.Students = studentsDto;
 
                 result.Add(courseObj);
             });
